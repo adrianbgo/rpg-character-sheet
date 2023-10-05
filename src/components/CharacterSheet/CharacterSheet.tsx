99323cf1c6db070d532de742729f10ee52b91e12
adrianbbauer <abauer@crsth.com>
2023-10-05 11:31:41 -0400

    bugfix: abstracted Character for future feature addition

--- src/components/CharacterSheet/CharacterSheet.tsx
+++ src/components/CharacterSheet/CharacterSheet.tsx
@@ -4,17 +4,12 @@ import Stat from "../Stat/Stat";
 import Skill from "../Skill/Skill";
 
 import "../../styles/components/CharacterSheet.scss";
-import { InitialAttributes, Attribute } from "../../types/Attribute";
 import { changeDerived } from "../../util/changeDerived";
-import { InitialSkills, SkillType } from "../../types/Skill";
+import { Character, InitialCharacter } from "../../types/Character";
 
 const CharacterSheet: React.FC = () => {
-    const [name, setName] = useState<string>("");
-    const [attributes, setAttributes] = useState<{ [key: string]: Attribute }>(
-        InitialAttributes
-    );
-    const [skills, setSkills] = useState<{ [key: string]: SkillType }>(InitialSkills);
-    changeDerived(attributes);
+    const [character, setCharacter] = useState<Character>(InitialCharacter);
+    changeDerived(character.attributes);
     return (
         <div className="container">
             <h1>Character Sheet</h1>
@@ -24,30 +19,33 @@ const CharacterSheet: React.FC = () => {
                         label="Character Name"
                         id="name"
                         placeholder="Johnny Appleseed"
-                        onChange={setName}
-                        value={name}
+                        onChange={(val) => setCharacter({ ...character, name: val })}
+                        value={character.name}
                     />
                 </section>
                 <section id="base-attributes">
                     <h2>Base Attributes</h2>
-                    {Object.keys(attributes).map((key: string) => {
-                        if (attributes[key].computed === false) {
+                    {Object.keys(character.attributes).map((key: string) => {
+                        if (character.attributes[key].computed === false) {
                             return (
                                 <Stat
                                     id={key}
                                     key={key}
                                     type={key}
-                                    computed={attributes[key].computed}
+                                    computed={character.attributes[key].computed}
                                     onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-                                        setAttributes({
-                                            ...attributes,
-                                            [key]: {
-                                                ...attributes[key],
-                                                value: e.target.valueAsNumber,
+                                        setCharacter({
+                                            ...character,
+                                            attributes: {
+                                                ...character.attributes,
+                                                [key]: {
+                                                    ...character.attributes[key],
+                                                    value: e.target.valueAsNumber,
+                                                }
                                             }
-                                        });
+                                        })
                                     }}
-                                    value={attributes[key].value}
+                                    value={character.attributes[key].value}
                                 />
                             );
                         }
@@ -56,14 +54,14 @@ const CharacterSheet: React.FC = () => {
                 </section>
                 <section id="combat-attributes">
                     <h2>Combat Attributes</h2>
-                    {Object.keys(attributes).map((key: string) => {
-                        if (attributes[key].computed === true) {
+                    {Object.keys(character.attributes).map((key: string) => {
+                        if (character.attributes[key].computed === true) {
                             return (
                                 <Stat
                                     key={key}
                                     type={key}
-                                    computed={attributes[key].computed}
-                                    value={attributes[key].value}
+                                    computed={character.attributes[key].computed}
+                                    value={character.attributes[key].value}
                                     onChange={() => { }}
                                     id={key}
                                 />
@@ -74,13 +72,13 @@ const CharacterSheet: React.FC = () => {
                 </section>
                 <section id="skills">
                     <h2>Skills</h2>
-                    {Object.keys(skills).map((key: string) => {
+                    {Object.keys(character.skills).map((key: string) => {
                         return (
                             <Skill
                                 key={key}
                                 name={key}
-                                attributes={attributes}
-                                skill={skills[key]}
+                                attributes={character.attributes}
+                                skill={character.skills[key]}
                             />
                         );
                     })}
@@ -94,9 +92,11 @@ const CharacterSheet: React.FC = () => {
                                 reader.onload = (e) => {
                                     if (e.target) {
                                         const result = JSON.parse(e.target.result as string);
-                                        setName(result.name);
-                                        setAttributes(result.attributes);
-                                        setSkills(result.skills);
+                                        setCharacter({
+                                            name: result.name,
+                                            attributes: result.attributes,
+                                            skills: result.skills
+                                        })
                                     }
                                 };
                                 if (e.target.files) {
@@ -105,7 +105,7 @@ const CharacterSheet: React.FC = () => {
                             }
                         } accept=".json" />
                     </label>
-                    <a className='button secondary' href={"data:text/json;chaset=utf-8," + JSON.stringify({ name, attributes, skills })} download="character.json">Export Character</a>
+                    <a className='button secondary' href={"data:text/json;chaset=utf-8," + JSON.stringify(character)} download="character.json">Export Character</a>
                 </section>
             </form>
         </div>
